1cd474f087d39eb764807c5a4155f91d28145608
Adib Zefar <adibzefar@gmail.com>
2022-09-18 20:11:29 -0400

    Refactoring our button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
 export default function Button(props) {
 
+  // let buttonClass = "button";
+
+  const buttonClass = classNames("button",{" button--confirm":props.confirm}, {" button--danger":props.danger})
+
+  classNames("button", {props: true});
+
+  return (
+    <button 
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+      >
+      {props.children}
+    </button>
+  );
 }
